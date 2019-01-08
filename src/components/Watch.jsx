42bf4e24a133d0b2e2f1ddc94a953b05559566b2
yuvicode <yuvalheftman@gmail.com>
2019-01-08 22:45:16 +0200

    add comments

--- src/components/Watch.jsx
+++ src/components/Watch.jsx
@@ -1,16 +1,24 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 import {Card, Col, CardTitle} from 'react-materialize'
 import {Video} from 'cloudinary-react'
-
+import Comments from 'recomment'
 class Watch extends Component {
   constructor(props) {
     super(props);
-    this.state = {videoInfo:null}
+    const comments = this.props.videoInfo.comments ? this.props.videoInfo.comments : []
+    this.state = {videoInfo:null, comments}
+    this.addComment = this.addComment.bind(this);
   }
   componentDidMount(){
-    this.props.videoService.getVideoInfo(this.props.videoInfoUrl).then(
+    this.props.videoService.getVideoInfo(this.props.videoInfo.url).then(
       videoInfo => this.setState({videoInfo})
     )
+  }
+  addComment(comment){
+    const comments = this.state.comments.map( comment => comment)
+    comments.push(comment)
+    this.props.videoInfo.comments = comments
+    this.setState({comments})
   }
   getVideoUrl(url,cloudinaryCloudName) {
     const fileName = url.split('/').pop()
@@ -23,11 +31,16 @@ class Watch extends Component {
     }
     return (  <Col m={6} s={12} >
       <Card className='blue-grey darken-1' title={data.name} textClassName='white-text'>
-        <video controls="controls" height="360" loop="loop" width="480">
-          <source src={this.getVideoUrl(data.url, this.props.cloudinaryCloudName)} type="video/mp4" />
-          Your browser does not support HTML5 video tags.
-        </video>
+        <div>
+          <video controls="controls" height="360" loop="loop" width="480">
+            <source src={this.getVideoUrl(data.url, this.props.cloudinaryCloudName)} type="video/mp4" />
+            Your browser does not support HTML5 video tags.
+          </video>
+        </div>
         <div>{data.description}</div>
+        <div>
+          <Comments onAddComment={this.addComment} comments={this.state.comments} />
+        </div>
       </Card>
 
     </Col>)
